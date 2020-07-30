6276f697c58be6d271c31f1c8c0338a6068339c9
yyt12345 <821729254@qq.com>
2020-07-30 16:52:56 +0800

    first

--- 059.cpp
+++ 059.cpp
@@ -3,11 +3,13 @@
  * @Author: Ye Yating
  * @Date: 2020-07-30 16:44:06
  * @LastEditors: Ye Yating
- * @LastEditTime: 2020-07-30 16:50:48
+ * @LastEditTime: 2020-07-30 16:52:28
  */
 #include <iostream>
 #include <vector>
 using namespace std;
+// 执行用时：0 ms, 在所有 C++ 提交中击败了100.00% 的用户
+// 内存消耗：6.8 MB, 在所有 C++ 提交中击败了16.28% 的用户
 vector<vector<int>> generateMatrix(int n)
 {
     vector<vector<int>> res(n, vector<int>(n));
