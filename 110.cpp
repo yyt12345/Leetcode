0705ea4a7920c37f352867c19c965533e26cac7f
yyt12345 <821729254@qq.com>
2020-08-17 10:36:02 +0800

    first

--- 110.cpp
+++ 110.cpp
@@ -1,20 +1,13 @@
 /*
- * @Descripttion: 
+ * @Descripttion:     110. 平衡二叉树
+        给定一个二叉树，判断它是否是高度平衡的二叉树。
+        本题中，一棵高度平衡二叉树定义为：
+            一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
  * @Author: Ye Yating
  * @Date: 2020-08-17 09:59:41
  * @LastEditors: Ye Yating
- * @LastEditTime: 2020-08-17 10:34:04
+ * @LastEditTime: 2020-08-17 10:35:47
  */
-/* 
-    110. 平衡二叉树
-
-    给定一个二叉树，判断它是否是高度平衡的二叉树。
-
-    本题中，一棵高度平衡二叉树定义为：
-
-        一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
-
-*/
 #include <iostream>
 using namespace std;
 struct TreeNode {
