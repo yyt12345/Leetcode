d0b7a43fc30f5b773abbb4aa6a0f6866950783c0
yyt12345 <821729254@qq.com>
2020-10-09 21:17:09 +0800

    141. 环形链表

--- 141.cpp
+++ 141.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: Ye Yating
  * @Date: 2020-10-09 20:06:08
- * @LastEditTime: 2020-10-09 20:45:01
+ * @LastEditTime: 2020-10-09 21:16:35
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /undefined/home/whh/programming/Leetcode/141.cpp
@@ -25,4 +25,20 @@ public:
         return false;
     }
 // 快慢指针
+// 执行用时：12 ms, 在所有 C++ 提交中击败了65.52% 的用户
+// 内存消耗：7.4 MB, 在所有 C++ 提交中击败了77.25% 的用户
+    bool hasCycle2(ListNode *head) {
+        if(head==nullptr || head->next==nullptr)    return false;
+        ListNode* slow,*quick;
+        slow=head;
+        quick=head->next;
+        while (quick!=slow)
+        {
+            if(fast==nullptr || fast->next==nullptr) return false;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return true;
+
+    }
 };
