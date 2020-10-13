601034b903d4a0a520ea44ebee7eca1cb1947c17
yyt12345 <821729254@qq.com>
2020-10-13 09:20:41 +0800

    修改

--- dependOn.h
+++ dependOn.h
@@ -1,21 +1,46 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-21 09:49:36
+ * @LastEditTime: 2020-10-13 09:05:30
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /undefined/home/whh/programming/Leetcode/dependOn.h
+ */
 
 #ifndef _DEPENDON_H_
 #define _DEPENDON_H_
+
+#include <queue>
 
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 struct ListNode {
     int val;
     ListNode* next;
     ListNode(int x)
         : val(x)
-        , next(NULL)
+        , next(nullptr)
     {
     }
 };
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(nullptr), right(nullptr), next(nullptr) {}
+
+    Node(int _val) : val(_val), left(nullptr), right(nullptr), next(nullptr) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
 
 #endif
