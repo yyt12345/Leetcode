0d3654d1ae6d30ebad0f8b8e8554b9fcc2720b5e
yyt12345 <821729254@qq.com>
2020-12-14 13:52:03 +0800

    数据流中的中位数

--- JZ63.cpp
+++ JZ63.cpp
@@ -1,12 +1,13 @@
 #include <vector>
 #include <iostream>
+#include <queue>
 using namespace std;
 class Solution {
 public:
     vector<int> nums;
     void Insert(int num)
     {
-        //���ַ�����
+        //二分法
         int end = nums.size() - 1, start = 0;
         while (start <= end) {
             int mid = (start+end)>>1;
@@ -27,4 +28,30 @@ public:
         if (n % 2) return nums[n / 2];
         else return (nums[n / 2] + nums[n / 2 - 1]) / 1.0;
     }
+// 使用优先队列
+    priority_queue<int> a;//大顶堆
+    priority_queue<int ,vector<int>, greater<int> > b;//小顶堆
+    void Insert2(int num){
+        a.push(num);
+
+        b.push(a.top());
+        a.pop();
+
+        if(a.size()<b.size()){
+            a.push(b.top());
+            b.pop();
+        }
+    }
+    double GetMedian2(){
+        return a.size()>b.size()?a.top():(a.top()+b.top())*0.5;
+    }
+
 };
+int main(){
+    Solution s;
+    vector<int> nums={5,2,3,4,1,6,7,0,8};
+    for(int i=0;i<nums.size();i++ ){
+        s.Insert2(nums[i]);
+        cout << s.GetMedian2() << endl;
+    }
+}
